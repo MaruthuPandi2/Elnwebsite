c03c60581912ea110bd51292980c54687eab8eda
Maruthu Pandi <mrtuad555@gmail.com>
2025-01-07 10:09:19 +0530

    your comments

--- app/api/download/route.ts
+++ app/api/download/route.ts
@@ -272,7 +272,8 @@ import fs from 'fs';
 import path from 'path';
 
 export async function GET() {
-  const filePath = path.join(process.cwd(), 'files/eln-brochure.pdf');
+  // const filePath = path.join(process.cwd(), 'files/eln-brochure.pdf');
+  const filePath = path.resolve('files', 'eln-brochure.pdf');
   const fileBuffer = fs.readFileSync(filePath);
 
   const response = new NextResponse(fileBuffer, {
