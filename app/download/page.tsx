b295882986b0eb1c3037ba815de76892d2886df6
Maruthu Pandi <mrtuad555@gmail.com>
2025-01-07 18:31:20 +0530

    your comments

--- app/download/page.tsx
+++ app/download/page.tsx
@@ -1,10 +1,12 @@
-import DownloadButton from '../../components/DownloadButton';
+//import DownloadButton from '../../components/DownloadButton';
+
+import DownloadButton from "@/components/downloadbutton";
 
 export default function DownloadPage() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>Download Your PDF</h1>
-      <DownloadButton />
+            <DownloadButton/>
     </div>
   );
 }
